9d61cd2e12e2b6e069ce1bd84edac8ebc503f41b
andor <latandor@gmail.com>
2022-06-30 17:41:33 -0400

    first commit

---
+++ fundamentals.js
@@ -0,0 +1,6 @@
+const header = document.getElementById(`header`)
+const p = document.getElementById(`para`)
+const ul = docuent.getElementById(`list-1`)
+
+const li = document.createElement(`li`)
+document.ul.append(liItem)
